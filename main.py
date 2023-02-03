02e2b8d8adc5b3436c8bbdc50066766ba5ddab88
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2023-02-03 17:37:48 +0900

    record

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
+import time, cv2
+from threading import Thread
 from djitellopy import Tello
-import time
 
-drone = Tello()
-drone.connect()
+tello = Tello()
 
-drone.takeoff()
-time.sleep(5)
-drone.land()
+tello.connect()
 
-# Path: requirements.txt
-djitellopy
+keepRecording = True
+tello.streamon()
+frame_read = tello.get_frame_read()
+
+
+def videoRecorder():
+    height, width, _ = frame_read.frame.shape
+    video = cv2.VideoWriter('video.avi', cv2.VideoWriter_fourcc(*'XVID'), 30, (width, height))
+
+    while keepRecording:
+        video.write(frame_read.frame)
+        time.sleep(1 / 30)
+
+    video.release()
+
+
+recorder = Thread(target=videoRecorder)
+recorder.start()
+
+tello.takeoff()
+tello.move_up(100)
+tello.rotate_counter_clockwise(360)
+tello.land()
+
+keepRecording = False
+recorder.join()
